efba6802f333ff731cf1c86a324fb0d371c77523
Antu Das <antu.haks@gmail.com>
2026-04-22 15:11:34 +0600

    Fix contact page merge regression

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,31 +1,17 @@
 import { ContactMap } from "@/components/contact/ContactMap";
+import FullPageWrapper from "@/utils/FullPageWrapper";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "CONTACT - HML"
-}
+    title: "CONTACT - HML",
+};
 
 export default function contactpage() {
     return (
-        <main className="pt-16 md:pt-20">
-            <ContactMap />
-            <Footer/> */}
-            {/* <div className="min-w-full h-screen overflow-y-scroll scroll-smooth snap-y snap-mandatory flex flex-col hide-scrollbar">
-                <section className="snap-always snap-start h-screen">
-                    <ContactHero />
-                </section>
-                <section className="snap-always snap-start h-screen">
-                    <ContactMap />
-                </section>
-            </div> */}
-            <FullPageWrapper>
-                {/* <div className="section">
-                    <ContactHero />
-                </div> */}
-                <div className="section">
-                    <ContactMap />
-                </div>
-            </FullPageWrapper>
-        </>
-    )
+        <FullPageWrapper>
+            <div className="section">
+                <ContactMap />
+            </div>
+        </FullPageWrapper>
+    );
 }
