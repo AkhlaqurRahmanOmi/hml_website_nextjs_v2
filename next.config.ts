24ec33b9d17878c6c7edc7ad85125dbe0752f728
HATechZ <hatechz.bd@gmail.com>
2026-04-29 10:47:52 +0600

    Added image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   trailingSlash: true,
   images: {
-    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
       },
     ],
   },
